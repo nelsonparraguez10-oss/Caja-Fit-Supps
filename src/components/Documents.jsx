89460d818b6f192502dc893f75a55a9b736f1e26
Nelson Parraguez <nelson.parraguez10@gmail.com>
2026-05-07 22:19:03 -0400

    feat: remove Notas de Venta tab, keep only Facturas y Boletas

--- src/components/Documents.jsx
+++ src/components/Documents.jsx
@@ -1,69 +1,28 @@
-import { useState } from 'react'
-import { SalesNote } from './SalesNote'
-import { Invoice }   from './Invoice'
-import { useProducts }   from '../hooks/useDB'
-import { useClients }    from '../hooks/useDB'
+import { Invoice }      from './Invoice'
+import { useProducts }  from '../hooks/useDB'
+import { useClients }   from '../hooks/useDB'
 import { useSalesNotes } from '../hooks/useDB'
-import { useDocuments }  from '../hooks/useDB'
+import { useDocuments } from '../hooks/useDB'
 
 export function Documents() {
-  const [subTab, setSubTab]         = useState('notas')
-  const [pendingNote, setPending]   = useState(null)
-
-  const { products }                                             = useProducts()
-  const { clients }                                              = useClients()
-  const { notes, create: createNote, update: updateNote, remove: removeNote } = useSalesNotes()
-  const { docs,  create: createDoc,  remove: removeDoc }         = useDocuments()
-
-  const handleEmitFromNote = (note) => {
-    setPending(note)
-    setSubTab('documentos')
-  }
+  const { products }                                     = useProducts()
+  const { clients }                                      = useClients()
+  const { notes, update: updateNote }                    = useSalesNotes()
+  const { docs, create: createDoc, remove: removeDoc }   = useDocuments()
 
   return (
     <div className="documents">
-      <div className="doc-tabs">
-        <button
-          type="button"
-          className={subTab === 'notas' ? 'active' : ''}
-          onClick={() => setSubTab('notas')}
-        >
-          NOTAS DE VENTA
-        </button>
-        <button
-          type="button"
-          className={subTab === 'documentos' ? 'active' : ''}
-          onClick={() => setSubTab('documentos')}
-        >
-          FACTURAS Y BOLETAS
-        </button>
-      </div>
-
-      {subTab === 'notas' && (
-        <SalesNote
-          products={products}
-          clients={clients}
-          notes={notes}
-          onCreate={createNote}
-          onUpdate={updateNote}
-          onRemove={removeNote}
-          onEmit={handleEmitFromNote}
-        />
-      )}
-
-      {subTab === 'documentos' && (
-        <Invoice
-          products={products}
-          clients={clients}
-          notes={notes}
-          docs={docs}
-          onCreate={createDoc}
-          onRemove={removeDoc}
-          onUpdateNote={updateNote}
-          prefillNote={pendingNote}
-          onClearPrefill={() => setPending(null)}
-        />
-      )}
+      <Invoice
+        products={products}
+        clients={clients}
+        notes={notes}
+        docs={docs}
+        onCreate={createDoc}
+        onRemove={removeDoc}
+        onUpdateNote={updateNote}
+        prefillNote={null}
+        onClearPrefill={() => {}}
+      />
     </div>
   )
 }
